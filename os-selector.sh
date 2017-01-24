340aa3c33780400257879e1e77c9fd19bda1afd7
RobClap <robclap8@gmail.com>
2017-01-24 10:11:03 +0100

    Added more general multi-os approach

--- os-selector.sh
+++ os-selector.sh
@@ -4,10 +4,10 @@ then
 	source "$(dirname $0)/linux.sourceme"
 	# If available, use LSB to identify distribution
 	if [ -f /etc/lsb-release -o -d /etc/lsb-release.d ]; then
-		export DISTRO=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
+		export DISTRO=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'// | tr '[:upper:]' '[:lower:]')
 		# Otherwise, use release info file
 	else
-		export DISTRO=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | head -n 1 | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1)
+		export DISTRO=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | head -n 1 | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1 | tr '[:upper:]' '[:lower:]')
 	fi
 	# For everything else (or if above failed), just use generic identifier
 	[[ "$DISTRO" == "" ]] && export DISTRO=$(uname)
